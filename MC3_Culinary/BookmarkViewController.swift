bfff9486220e9d0730a209b3ea0435399ee8bd3e
Leonnardo Benjamin Hutama <leonnardo.hutama@binusa.c.id>
2019-08-20 11:43:31 +0700

    Template and ViewController File for each page

---
+++ MC3_Culinary/BookmarkViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BookmarkViewController.swift
+//  MC3_Culinary
+//
+//  Created by Leonnardo Benjamin Hutama on 20/08/19.
+//  Copyright © 2019 Leonnardo Benjamin Hutama. All rights reserved.
+//
+
+import UIKit
+
+class BookmarkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
