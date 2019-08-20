5ea30c11e646125927b0f860167205f4ce5605b0
Leonnardo Benjamin Hutama <leonnardo.hutama@binusa.c.id>
2019-08-20 09:20:12 +0700

    Initial Commit

---
+++ MC3_Culinary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MC3_Culinary
+//
+//  Created by Leonnardo Benjamin Hutama on 20/08/19.
+//  Copyright © 2019 Leonnardo Benjamin Hutama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
