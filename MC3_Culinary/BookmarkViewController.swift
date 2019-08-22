ea33b8b313c1e76f2345662c12c6d34056b58471
Leonnardo Benjamin Hutama <leonnardo.hutama@binusa.c.id>
2019-08-22 11:53:30 +0700

    bookmark layout

--- MC3_Culinary/BookmarkViewController.swift
+++ MC3_Culinary/BookmarkViewController.swift
@@ -8,15 +8,50 @@
 
 import UIKit
 
-class BookmarkViewController: UIViewController {
-
+class BookmarkViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var bookmarkedFood = ["Sate", "Rendang", "Bakso"]
+    var bookmarkSubtitle = ["Sumatera", "jawa", "Padang"]
+    var bookmarkImage: [UIImage] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bookmarkedFood.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookmarkCell", for: indexPath)
+        cell.textLabel?.text = bookmarkedFood[indexPath.item]
+        cell.detailTextLabel?.text = bookmarkSubtitle[indexPath.item]
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let moveObjTemp = bookmarkedFood[sourceIndexPath.item]
+        bookmarkedFood.remove(at: sourceIndexPath.item)
+        bookmarkedFood.insert(moveObjTemp, at: destinationIndexPath.item)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            bookmarkedFood.remove(at: indexPath.item)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func btnEditClicked(_ sender: UIBarButtonItem) {
+        self.tableView.isEditing = !self.tableView.isEditing
+        sender.title = (self.tableView.isEditing) ? "Done" : "Edit"
+    }
+    
     /*
     // MARK: - Navigation
 
